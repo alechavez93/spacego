7222f8340bcba123448da4ef5033e3722c990c0b
Alejandro Chavez <alechavez93@gmail.com>
2017-01-20 22:31:32 -0500

    Deployment test 4

--- server.js
+++ server.js
@@ -7,7 +7,9 @@
 var express = require("express");
 var app = express();
 app.get("/", function (req, res) {
-    res.sendFile(__dirname + "/index.html");
+    res.send("This is a test response\n" +
+        "You have successfully sent an http request to spacego API\n" +
+        "This is what you sent: \n" + JSON.stringify(req));
 });
 app.listen(process.env.PORT || 3000);
 //# sourceMappingURL=server.js.map
--- server.ts
+++ server.ts
@@ -9,7 +9,9 @@ let express = require("express");
 let app = express();
 
 app.get("/", function(req, res){
-   res.sendFile(__dirname+"/index.html");
+   res.send("This is a test response\n" +
+       "You have successfully sent an http request to spacego API\n" +
+       "This is what you sent: \n" + JSON.stringify(req));
 });
 
 app.listen(process.env.PORT || 3000);
