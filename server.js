a0f26948a70a75ec7564eec343b08f07f03c4bf3
Alejandro Chavez <alechavez93@gmail.com>
2017-01-21 19:22:17 -0500

    API now returns JSON data

--- server.js
+++ server.js
@@ -14,16 +14,10 @@ app.get("/", function (req, res) {
     res.sendFile(__dirname + "/index.html");
 });
 app.get("/mainAPI", function (req, res) {
-    res.send("This is a test response\n" +
-        "You have successfully sent an http request to spacego API\n" +
-        "This is what you sent (Headers): \n" + JSON.stringify(req.headers) + "\n\n" +
-        "-Ale");
+    res.send(req.headers);
 });
 app.post("/mainAPI", function (req, res) {
-    res.send("This is a test response for POST http requests\n" +
-        "The Headers were: \n" + JSON.stringify(req.headers) + "\n\n" +
-        "The Body was: \n" + JSON.stringify(req.body) + "\n\n" +
-        "-Ale");
+    res.send(req.body);
 });
 app.listen(process.env.PORT || 4000);
 //# sourceMappingURL=server.js.map
